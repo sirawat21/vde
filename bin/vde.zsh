17c9e915de14a9eff971adcf60593fad62f8fb83
Sira <sirawat21@gmail.com>
2025-08-28 14:15:53 +1000

    Added vde exec shell

--- bin/vde.zsh
+++ bin/vde.zsh
@@ -1,5 +1,15 @@
 #/bin/zsh
 
-# function vde-node() {
-#   docker run --rm -it -v "$(pwd)":/app node-cli "$@"
-# }
+function vde-ls() {
+    echo "::: Available VDE Images :::"
+    docker image ls --filter "reference=vde-*"
+}
+
+function vde() {
+    docker run --rm -it \
+    $1 \
+    --name "$1-container" \
+    -v "$(pwd)":/app \
+    node-cli \
+    "${@:2}"
+}
